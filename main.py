3c710211c7cf2c96d50e42fc034e0e1c3a521273
maxsilc2 <maxsilc2@gmail.com>
2023-03-07 04:01:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!pip install pyTelegramBotAPI
-#pip install requests
-#pip install Pillow
+#!pip install requests
+#!pip install Pillow
 
 import time
 import telebot
@@ -9,7 +9,10 @@ from PIL import Image
 import requests
 from io import BytesIO
 import random
+import array
 
+people = ["янина новицкая", "алина соловьева", "полина крутских","виктория крихели","настя винниченко","дина павлова","кристина проскурякова","полина леженина","вика одинцова","женя бажутова","марета","лена минченко","маша троя","катя ???","анна красавина","нургуль мартс","натали салий","марика","марина левочкина","марина романова","ирина микоян","екатерина кутумова","елена мариева","света сегеда","мария шишова","олеся панова","юля мошкович"]
+code = ["VBRE3CJD","UT5BQZ4N","649H82EN","EYP8EWAG","GQJSCKMH","APAMYQ2P","QX7GT6VS","VNUZQC4H","4QW9A7ER","WQFYHGR2","T6JRYX28","G8DX8NZ2","KSADA8F9","HK5ABAYW","Q2CZMJ92","95QGX7HM","TRZS22QU","M52EU3PV","DXVD3YF8","FJ5JA3JF","VBRE3CJD","M5SXG6NG","8Q2FMVAF","6QXKXPF4","4PTYDXAU","T3RMR9BJ","WW7JJNHN"]
 TOKEN = "6024345369:AAF__ShNzfEBIoKVB3tNETNqZNcc7-CRuWY"
 bot = telebot.TeleBot(TOKEN)
 
@@ -17,12 +20,18 @@ def getargs(text):
 	_args = text.split()[1:]
 	return _args
 
+def download_file(url, name="video.mp4"):
+    r = requests.get(url, stream=True)
+    with open(name, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
 
 @bot.message_handler(commands=["start"])
 def get_user_info(message):
 
-  markup_inline2 = types.InlineKeyboardMarkup()
-  item_0 = types.InlineKeyboardButton(text = '🍀', callback_data = '1')
+  markup_inline2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  item_0 = types.KeyboardButton('🍀')
   markup_inline2.add(item_0)
 
   bot.send_message(message.chat.id, "✨Дорогая, вся наша женская команда поздравляет тебя с праздником весны!✨\n\
@@ -33,42 +42,39 @@ def get_user_info(message):
 \n\
 Удачи!", reply_markup = markup_inline2)
 
-  #bot.send_message(message.chat.id,"Хотите получить личное предсказание на 2023 год? Нажимайте рандомную кнопку, чтобы выбрать предсказание и переходите по ссылке)", 
-                   #reply_markup = markup_inline)
+@bot.message_handler(content_types=['text'])
+def get_text(message):
+  if message.text == '🍀':
+       markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+       item_1 = types.KeyboardButton('❤️')
+       markup_reply.add(item_1) 
+       bot.send_message(message.chat.id,"Вращай Колесо Фортуны и выигрывай незабываемые впечатления!")
+       download_file("https://scontent.cdninstagram.com/o1/v/t16/f1/m82/9749A770FEB768336D57A6EB96EC3F83_video_dashinit.mp4?efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uNzIwLmNsaXBzLmJhc2VsaW5lIn0&_nc_ht=instagram.ftas2-1.fna.fbcdn.net&_nc_cat=104&vs=1276778489591918_578504870&_nc_vs=HBksFQIYT2lnX3hwdl9yZWVsc19wZXJtYW5lbnRfcHJvZC85NzQ5QTc3MEZFQjc2ODMzNkQ1N0E2RUI5NkVDM0Y4M192aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVABgkR0hQMTlCUEhrbU1IMzFZRUFNWDZSODdUMUVZbGJxX0VBQUFGFQICyAEAKAAYABsAFQAAJurYruib2fo%2FFQIoAkMzLBdARQQ5WBBiThgSZGFzaF9iYXNlbGluZV8xX3YxEQB1%2FgcA&_nc_rid=1e89be8e90&ccb=9-4&oh=00_AfCAu0UkybzbJ4U8PklZm8fiDBpZeZreVGBXhOrll2g-ig&oe=64083A9F&_nc_sid=30a2ef", "fortune.mp4")
+       vidnet = open("fortune.mp4", "rb")
+       bot.send_video(message.chat.id, vidnet)
+       bot.send_message(message.chat.id,"Ура! Ты выиграла главный приз!")
+       bot.send_message(message.chat.id,"Пришли нам в ответ на это сообщение - сердечко ❤️ и порадуйся вместе с нами!", 
+                      reply_markup = markup_reply)
+  elif message.text == '❤️':
+       bot.send_message(message.chat.id,"Успешной женщине, всегда улыбается удача)")
+       bot.send_message(message.chat.id,"Пришли свое имя и фамилию русскими буквами (например: Мирослава Байда), чтобы получить свой Персональный подарок ")
+  elif message.text.lower() == 'анастасия колтунова' or message.text.lower() == 'максим си' or message.text.lower() == 'макси':
+       bot.send_message(message.chat.id, 'Привет создатель!')
+  i=0
+  while message.text.lower() != people[i]:
+             i += 1                 
+  if i<=26:
+        bot.send_message(message.chat.id, 'Поздравляем!!!')
+        bot.send_message(message.chat.id, 'Этот подарок создан специально для тебя!')
+        bot.send_message(message.chat.id, 'Регистрируйся на понравившийся курс от наших друзей школы Masters используя данный промокод!')
+        bot.send_message(message.chat.id, code[i] )
+        bot.send_message(message.chat.id, 'Получай море вдохновения и весеннего настроения вместе с командой Dazzz.')
+  else:
+        bot.send_message(message.chat.id, 'Я не нашел такого имени, попробуй ввести полное имя или сокращенное🥺')
 
-@bot.callback_query_handler(func = lambda call: True)
-def answer(call):
-  markup_inline3 = types.InlineKeyboardMarkup()
-  item_0 = types.InlineKeyboardButton(text = '❤️', callback_data = '1')
-  markup_inline3.add(item_0)
-  if call.data == '1':
-    bot.send_message(message.chat.id, "Вращай "Колесо Фортуны" и выигрывай незабываемые впечатления!\n\
-Ура! Ты выиграла главный приз!\n\
-Пришли нам в ответ на это сообщение - сердечко ❤️ и порадуйся вместе с нами!", reply_markup = markup_inline3)  
-  elif call.data == '2':
-    pass
-@bot.callback_query_handler(func = lambda call: True)
-def answer(call):
-if call.data == '1':
-    bot.send_message(message.chat.id, "Вращай "Колесо Фортуны" и выигрывай незабываемые впечатления!\n\
-Ура! Ты выиграла главный приз!\n\
-Пришли нам в ответ на это сообщение - сердечко ❤️ и порадуйся вместе с нами!", reply_markup = markup_inline3)  
-  elif call.data == '2':
-    pass
+    
 
 
-@bot.message_handler(commands=["helpmepls"])
-def sayhelp(message):
-	bot.send_message(message.chat.id, "There is no help. How dare are you, pig.")
-
-@bot.message_handler(commands=["saythis"])
-def textto_speech(message):
-	args = getargs(message.text)
-	text = " ".join(args[0:])
-	try:
-		bot.send_message(message.chat.id, text)
-	except Exception as e:
-		bot.send_message(message.chat.id, f"There was an exception")
 
 while True:
 	try:
